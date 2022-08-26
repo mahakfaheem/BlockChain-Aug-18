6a571e5f4e30794f081bdbef7a0a7a8ab0e20d3e
mahakfaheem <mahak.faheem19@st.niituniversity.in>
2022-08-26 19:41:42 +0530

    Smart Contract-3

---
+++ sm3.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.8.16;
+contract Student {
+    struct stu {
+        string name;
+        string regn;
+        string gender;
+        string enlyear; 
+    }
+    stu st1;
+
+    function setValue(string memory _name, string memory _regn, string memory _gender, string memory _enlyear) public {
+        st1 = stu(_name, _regn, _gender, _enlyear);
+    }
+
+    function setName(string memory _name) public {
+        st1.name = _name;
+    }
+
+    function setRegn(string memory _regn) public {
+        st1.regn = _regn;
+    }
+
+    function setEnlyear(string memory _enlyear) public {
+        st1.enlyear = _enlyear;
+    }
+
+    function setGender(string memory _gender) public {
+        st1.gender = _gender;
+    }
+
+    
+
+    
+
+    function getName() public view returns(string memory) {
+        return st1.name;
+    }
+
+    function getEnlyear() public view returns(string memory) {
+        return st1.enlyear;
+    }
+
+    function getGender() public view returns(string memory) {
+        return st1.gender;
+    }
+
+    function getRegn() public view returns(string memory) {
+        return st1.regn;
+    }
+}
